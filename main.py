9ac56f055220506a484a306926d0959f7d96300c
Andy <aasecchi@gmail.com>
2024-06-03 22:17:56 -0300

    Correción menor

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ from flask_cors import CORS, cross_origin
 import pandas as pd
 pd.options.plotting.backend = "plotly"
 
-
-app = Flask(__name__)
-cors = CORS(app)
-@app.route("/api/ratio", methods=['POST','OPTIONS'])
-@cross_origin()
-def ratio():
-    share=request.get_json("share1")
-    share_1=share["Share1"]
-    share_2=share["Share2"]
-    tickers = [share_1,share_2]
-    filtered_df=ratio_Calculator(tickers)
-    fig=filtered_df.plot()
-    graphJson=plotly.io.to_json(fig,pretty=True)
-    return graphJson
-
+def backend():
+    app = Flask(__name__)
+    cors = CORS(app)
+    @app.route("/api/ratio", methods=['POST','OPTIONS'])
+    @cross_origin()
+    def ratio():
+        share=request.get_json("share1")
+        share_1=share["Share1"]
+        share_2=share["Share2"]
+        tickers = [share_1,share_2]
+        filtered_df=ratio_Calculator(tickers)
+        fig=filtered_df.plot()
+        graphJson=plotly.io.to_json(fig,pretty=True)
+        return graphJson
+    return app
 
 if __name__=="__main__":
+    app=backend()
     app.run()
 
